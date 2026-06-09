bfc96a33d9a77e0be308651e81d54deb3884fb09
jruano-espol <jruano@espol.edu.ec>
2026-06-08 23:06:39 -0500

    setup the frontend

--- certipro_backend/settings.py
+++ certipro_backend/settings.py
@@ -8,24 +8,36 @@ from urllib.parse import urlparse, parse_qsl
 
 load_dotenv()
 
-tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
-        'USER': tmpPostgres.username,
-        'PASSWORD': tmpPostgres.password,
-        'HOST': tmpPostgres.hostname,
-        'PORT': 5432,
-        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
-    }
-}
-
-AUTH_USER_MODEL = 'rest_api.User'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# When working only on the front-end it is annoying to have to set
+# up the postgres database, so it is defaulted to sqlite3 when it is
+# not available.
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL != None:
+    tmpPostgres = urlparse(DATABASE_URL)
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': tmpPostgres.path.replace('/', ''),
+            'USER': tmpPostgres.username,
+            'PASSWORD': tmpPostgres.password,
+            'HOST': tmpPostgres.hostname,
+            'PORT': 5432,
+            'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+AUTH_USER_MODEL = 'rest_api.User'
 
 
 # Quick-start development settings - unsuitable for production
@@ -68,7 +80,7 @@ ROOT_URLCONF = "certipro_backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "frontend/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
---
+++ frontend/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def landing_page(request):
+    context = {}
+    return render(request, "select_profile.html", context)
