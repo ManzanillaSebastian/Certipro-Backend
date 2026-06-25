f529d0e64e7c31b3e94fb6aabfc2e48d47910dd0
ManzanillaSebastián <sebmanza@espol.edu.ec>
2026-06-24 21:58:23 -0500

    BugFix: missing priority field on criteria serializer

--- rest_api/serializers/criteria.py
+++ rest_api/serializers/criteria.py
@@ -9,4 +9,4 @@ class CriterionSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Criterion
-        fields = ['id', 'code', 'title', 'description', 'certification_model', 'parent']
+        fields = ['id', 'code', 'title', 'priority', 'description', 'certification_model', 'parent']
