2368dda2c84342fa8958d31225a807d28163bd1f
ManzanillaSebastián <sebmanza@espol.edu.ec>
2026-06-29 19:50:06 -0500

    Modified: getting the project ready for deployment

--- certipro_backend/settings.py
+++ certipro_backend/settings.py
@@ -79,7 +79,6 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
